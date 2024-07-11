a4324d1fb7dac28e34825d135da882098637f8d0
Kasim Ahmic <kasim.ahmic@gmail.com>
2024-07-11 00:56:30 -0400

    chore: Add README

---
+++ README.md
@@ -0,0 +1,15 @@
+# lib-native
+
+This is a simple library that will someday (hopefully) provide a 1 to 1 mapping of the Windows API to JavaScript. This is a work in progress and is not yet ready for use.
+
+# How?
+
+I leverage the [`ffi-rs`](https://www.npmjs.com/package/ffi-rs) library to provide a foreign function interface that allows me to call into the Windows API.
+
+# Why?
+
+I was bored.
+
+# When will this be ready?
+
+Who knows. Probably never. Hopfully someday.
--- src/util/logger.ts
+++ src/util/logger.ts
@@ -10,14 +10,16 @@ const formatter: Intl.DateTimeFormat = new Intl.DateTimeFormat('en-US', {
   hour12: false,
 });
 
-const colorMap: Record<string, Parameters<typeof styleText>[0]> = {
-  DEBUG: ['magenta'],
+const colorMap: Record<string, Modifiers> = {
+  DEBUG: ['bgYellow', 'black'],
   LOG: ['green'],
   WARN: ['yellow'],
   ERROR: ['red'],
   FATAL: ['white', 'bold'],
   VERBOSE: ['cyan'],
 };
+
+type Modifiers = Parameters<typeof styleText>[0];
 
 export class Logger {
   private readonly name: string;
@@ -51,13 +53,14 @@ export class Logger {
   }
 
   private write(level: keyof typeof colorMap, message: string): void {
-    const modifiers = colorMap[level];
+    const modifiers = process.env.NO_COLOR ? [] : colorMap[level];
+    const nameModifiers: Modifiers = process.env.NO_COLOR ? [] : ['yellow'];
 
-    const lib = styleText(modifiers, '[libos]');
+    const lib = styleText(modifiers, '[lib-native]');
     const pid = styleText(modifiers, process.pid.toString());
     const timestamp = formatter.format(new Date());
     const lvl = styleText(modifiers, level.padStart(7));
-    const name = styleText(['yellow'], `[${this.name}]`);
+    const name = styleText(nameModifiers, `[${this.name}]`);
     const msg = styleText(modifiers, message);
 
     process.stdout.write(`${lib} ${pid} - ${timestamp} ${lvl} ${name} ${msg}\n`);
