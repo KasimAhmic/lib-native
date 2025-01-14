491303cb326b20af48f83465893144cf58645065
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-01-13 22:03:53 -0500

    feat: Add button styles to CreateWindowEx

--- src/win32/user32/create-window-ex.ts
+++ src/win32/user32/create-window-ex.ts
@@ -105,6 +105,23 @@ export enum ButtonStyle {
   RIGHTBUTTON = LEFTTEXT,
 }
 
+export enum EditStyle {
+  LEFT = 0x0000,
+  CENTER = 0x0001,
+  RIGHT = 0x0002,
+  MULTILINE = 0x0004,
+  UPPERCASE = 0x0008,
+  LOWERCASE = 0x0010,
+  PASSWORD = 0x0020,
+  AUTOVSCROLL = 0x0040,
+  AUTOHSCROLL = 0x0080,
+  NOHIDESEL = 0x0100,
+  OEMCONVERT = 0x0400,
+  READONLY = 0x0800,
+  WANTRETURN = 0x1000,
+  NUMBER = 0x2000,
+}
+
 export enum WindowPosition {
   USE_DEFAULT = 0x80000000,
 }
@@ -113,7 +130,7 @@ export function CreateWindowEx(
   extendedWindowStyle: ExtendedWindowStyle,
   className: LongPointerToConstantWideString,
   windowName: LongPointerToConstantWideString,
-  style: WindowStyle,
+  style: WindowStyle | ButtonStyle | EditStyle,
   xPosition: WindowPosition | number,
   yPosition: WindowPosition | number,
   width: WindowPosition | number,
