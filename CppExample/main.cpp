afd318c1c8203966b231ac6c191b3034f734d2a8
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-01-13 21:07:31 -0500

    feat: Convert all functions to Koffi and add all function necessary to create a window

--- CppExample/main.cpp
+++ CppExample/main.cpp
@@ -1,83 +1,111 @@
-#ifndef UNICODE
-#define UNICODE
-#endif
-
 #include <windows.h>
+#include <stdio.h>
+#include <uxtheme.h>
+#include <commctrl.h>
 
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+#pragma comment(lib, "uxtheme.lib")
+#pragma comment(lib, "Comctl32.lib")
 
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
-{
-	// Register the window class.
-	const wchar_t CLASS_NAME[] = L"Sample Window Class";
 
-	WNDCLASS wc = {};
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+	switch (msg) {
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProcW(hwnd, msg, wParam, lParam);
+	}
+	return 0;
+}
 
-	wc.lpfnWndProc = WindowProc;
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+	AllocConsole(); // Attach a console window
+
+	FILE* stream;
+	freopen_s(&stream, "CONOUT$", "w", stdout); // Redirect stdout to the console
+
+	INITCOMMONCONTROLSEX icex = { sizeof(INITCOMMONCONTROLSEX), ICC_STANDARD_CLASSES };
+	InitCommonControlsEx(&icex);
+
+	const wchar_t CLASS_NAME[] = L"MyWindowClass";
+
+	WNDCLASSEX wc = { };
+
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = WndProc;
 	wc.hInstance = hInstance;
+	wc.hCursor = LoadCursorW(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
 	wc.lpszClassName = CLASS_NAME;
 
-	RegisterClass(&wc);
+	if (!RegisterClassExW(&wc)) {
+		MessageBoxW(NULL, L"Window Registration Failed!", L"Error!", MB_ICONEXCLAMATION | MB_OK);
+		return 0;
+	}
 
-	// Create the window.
-
-	HWND hwnd = CreateWindowEx(
-			0,													 // Optional window styles.
-			CLASS_NAME,									 // Window class
-			L"Learn to Program Windows", // Window text
-			WS_OVERLAPPEDWINDOW,				 // Window style
-
-			CW_USEDEFAULT, //
-			CW_USEDEFAULT, //
-			CW_USEDEFAULT, //
-			CW_USEDEFAULT, //
-
-			NULL,									 // Parent window
-			NULL,									 // Menu
-			GetModuleHandle(NULL), // Instance handle
-			NULL									 // Additional application data
+	HWND hwnd = CreateWindowExW(
+		0,                              // Optional window styles
+		CLASS_NAME,                     // Window class
+		L"My Window",                   // Window text
+		WS_OVERLAPPEDWINDOW,            // Window style
+		CW_USEDEFAULT,                  // Position X
+		CW_USEDEFAULT,                  // Position y
+		500,                            // Width
+		300,                            // Height
+		NULL,                           // Parent window
+		NULL,                           // Menu
+		hInstance,                      // Instance handle
+		NULL                            // Additional application data
 	);
 
-	if (hwnd == NULL)
-	{
+	SetWindowTheme(hwnd, L"Explorer", NULL);
+
+	HWND okButtonHandle = CreateWindowExW(
+		0,
+		L"BUTTON",
+		L"OK",
+		WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+		10,
+		10,
+		100,
+		25,
+		hwnd,
+		NULL,
+		hInstance,
+		NULL
+	);
+
+	HWND cancelButtonHandle = CreateWindowExW(
+		0,
+		L"BUTTON",
+		L"Cancel",
+		WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
+		120,
+		10,
+		100,
+		25,
+		hwnd,
+		NULL,
+		hInstance,
+		NULL
+	);
+
+	printf("hInstance: %p\n", (void*)hInstance);
+
+	if (hwnd == NULL) {
+		MessageBox(NULL, L"Window Creation Failed!", L"Error!", MB_ICONEXCLAMATION | MB_OK);
 		return 0;
 	}
 
 	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
 
-	// Run the message loop.
-
-	MSG msg = {};
-	while (GetMessage(&msg, NULL, 0, 0) > 0)
-	{
+	MSG msg;
+	while (GetMessageW(&msg, NULL, 0, 0)) {
 		TranslateMessage(&msg);
-		DispatchMessage(&msg);
+		DispatchMessageW(&msg);
 	}
 
-	return 0;
-}
-
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
-{
-	switch (uMsg)
-	{
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		return 0;
-
-	case WM_PAINT:
-	{
-		PAINTSTRUCT ps;
-		HDC hdc = BeginPaint(hwnd, &ps);
-
-		// All painting occurs here, between BeginPaint and EndPaint.
-
-		FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
-
-		EndPaint(hwnd, &ps);
-	}
-		return 0;
-	}
-
-	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	return (int)msg.wParam;
 }
