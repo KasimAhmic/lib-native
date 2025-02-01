f03e4308ce8d4638f13900b2767abb1b4a6b4a3d
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-01-31 23:01:30 -0500

    feat: Add CharUpper functions

---
+++ src/win32/user32/char-upper.ts
@@ -0,0 +1,45 @@
+/**
+ * See the documentation for CharLower(W/A) for my gripes with these functions.
+ */
+import koffi from 'koffi';
+
+import { LPSTR, LPWSTR, WORD } from '../../@types';
+import {
+  ansiStringToString,
+  stringToAnsiBuffer,
+  stringToUnicodeBuffer,
+  unicodeStringToString,
+} from '../../util/type.util';
+import { user32 } from './user32';
+
+export function CharUpperW(character: string): string {
+  if (character.length === 1) {
+    const inputCharCode = character.charCodeAt(0);
+
+    const outputCharCode = user32.invoke('CharUpperW', WORD, [WORD], [inputCharCode], 'CharUpperWChar');
+
+    return String.fromCharCode(outputCharCode);
+  } else {
+    const buffer = stringToUnicodeBuffer(character);
+
+    user32.invoke('CharUpperW', LPWSTR, [koffi.inout(LPWSTR)], [buffer], 'CharUpperWString');
+
+    return unicodeStringToString(buffer);
+  }
+}
+
+export function CharUpperA(character: string): string {
+  if (character.length === 1) {
+    const inputCharCode = character.charCodeAt(0) & 0xff;
+
+    const outputCharCode = user32.invoke('CharUpperA', WORD, [WORD], [inputCharCode], 'CharUpperAChar');
+
+    return String.fromCharCode(outputCharCode);
+  } else {
+    const buffer = stringToAnsiBuffer(character);
+
+    user32.invoke('CharUpperA', LPSTR, [koffi.inout(LPSTR)], [buffer], 'CharUpperAString');
+
+    return ansiStringToString(buffer);
+  }
+}
