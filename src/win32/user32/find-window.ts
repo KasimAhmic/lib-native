34f916f8fa8867d791b05362e69b356706ef031c
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-02-11 20:23:34 -0500

    refactor: Make FindWindow return type Bool

--- src/win32/user32/find-window.ts
+++ src/win32/user32/find-window.ts
@@ -1,10 +1,10 @@
-import { HWND, LPCSTR, LPCWSTR } from '../../@types';
+import { HWND, LPCSTR, LPCWSTR, WindowHandle } from '../../@types';
 import { user32 } from './user32';
 
-export function FindWindowW(className: string | null, windowName: string | null): number {
+export function FindWindowW(className: string | null, windowName: string | null): WindowHandle {
   return user32.invoke('FindWindowW', HWND, [LPCWSTR, LPCWSTR], [className, windowName]);
 }
 
-export function FindWindowA(className: string | null, windowName: string | null): number {
+export function FindWindowA(className: string | null, windowName: string | null): WindowHandle {
   return user32.invoke('FindWindowA', HWND, [LPCSTR, LPCSTR], [className, windowName]);
 }
