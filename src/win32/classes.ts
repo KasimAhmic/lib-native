1df235de4a8faa703980c0cc5962092bb05763cf
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-02-12 20:23:35 -0500

    feat: Add combobox class and export macros and classes

--- src/win32/classes.ts
+++ src/win32/classes.ts
@@ -1,3 +1,4 @@
 export const EDIT_CLASS_NAME = 'EDIT';
 export const BUTTON_CLASS_NAME = 'BUTTON';
 export const STATUS_CLASS_NAME = 'msctls_statusbar32';
+export const COMBOBOX_CLASS_NAME = 'COMBOBOX';
--- src/win32/index.ts
+++ src/win32/index.ts
@@ -1,3 +1,5 @@
 export * from './constants';
+export * from './classes';
 export * from './library';
 export * from './structs';
+export * from './macros';
