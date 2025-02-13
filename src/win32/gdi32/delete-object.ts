a8e30918a0cdd088b71df4bf0db87a494dd0cc03
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-02-12 20:22:01 -0500

    feat: Allow other handles in DeleteObject function

--- src/win32/gdi32/delete-object.ts
+++ src/win32/gdi32/delete-object.ts
@@ -1,6 +1,6 @@
-import { BOOL, Bool, FontHandle, GDIObjectHandle, HGDIOBJ } from '../../@types';
+import { BOOL, Bool, BrushHandle, FontHandle, GDIObjectHandle, HGDIOBJ, PenHandle } from '../../@types';
 import { gdi32 } from './gdi32';
 
-export function DeleteObject(objectHandle: GDIObjectHandle | FontHandle): Bool {
+export function DeleteObject(objectHandle: GDIObjectHandle | FontHandle | PenHandle | BrushHandle): Bool {
   return gdi32.invoke('DeleteObject', BOOL, [HGDIOBJ], [objectHandle]);
 }
